25b6d57a9cb2033e1d004b329b888860fbb7b9c3
techno-dev18 <technologix2op@gmail.com>
2026-08-30 12:29:43 +0530

    added feature

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,22 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(
-            process.env.MONGO_URI,
-            {
-                family: 4
-            }
-        );
+        await mongoose.connect(process.env.MONGO_URI, {
+            family: 4,
+            serverSelectionTimeoutMS: 10000
+        });
 
-        console.log(
-            `MongoDB Connected: ${connection.connection.host}`
-        );
+        console.log("MongoDB Connected Successfully");
     } catch (error) {
-        console.error(
-            "MongoDB Connection Error:",
-            error.message
-        );
-
+        console.error("MongoDB Connection Error:", error.message);
         process.exit(1);
     }
 };
